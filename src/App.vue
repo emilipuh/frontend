84193d683ff96d338afdbd9a68d3296019e02778
Emili Puh <epuh@unipu.hr>
2024-01-17 19:55:55 +0100

    Napravljen 'kostur' navbara, postavljen backgroung image

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="fixed-bottom bg-body-tertiary">
+    <div class="container-fluid">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          Korisnik
+        </li>
+        <li class="nav-item">
+          Dodaj
+        </li>
+        <li class="nav-item">
+          Izbornik
+        </li>
+      </ul>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
 #app {
+  background-image: url('@/assets/pozadina.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  margin: 0;
+  overflow: hidden;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -16,15 +34,19 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 20px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
 
   a {
     font-weight: bold;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
+}
+
+.navbar-nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
 }
 </style>
