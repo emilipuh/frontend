c91dddcc9628886629bfba4d1e744e36143f5d96
Emili Puh <epuh@unipu.hr>
2024-02-12 15:43:45 +0100

    Spojeni frontend i backend (fetch), prikaz hardkodiranih podataka iz backenda

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
     <div id="stanje">
-      <h5>STANJE RAČUNA:</h5>
-      <h6 id="iznos">{{ stanje.stanjeRacuna }} €</h6>
+      <h5 style="padding: 0">STANJE RAČUNA:</h5>
+      <h6 style="margin-bottom: 0">{{ stanje.stanjeRacuna }} €</h6>
+      <!-- uvijek mora biti ispisana nula!!! -->
     </div>
     <div class="container-view">
       <div id="prihodi">
         <h6>PRIHODI: {{ stanje.prihodi }} €</h6>
+        <!-- čitamo sa backenda -->
       </div>
       <div id="rashodi">
         <h6>RASHODI: {{ stanje.rashodi }} €</h6>
@@ -28,11 +30,21 @@ export default {
   name: "Home",
   components: {
     NavbarIzbornik,
-},
+  },
   data: function () {
     return {
-      stanje,
+      stanje: {},
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/") // vraća promise
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Podaci s backenda: ", data);
+        this.stanje = data; // tu izjednačavamo podatke s backenda i frontenda
+      });
   },
 };
 </script>
@@ -42,18 +54,22 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  margin-top: 5vh;
 }
 
 #stanje {
   background-color: #066995;
   border-radius: 15px;
-  margin: 5vh 3vh;
+  padding: 3vh;
+  margin-left: 3vh;
+  margin-right: 3vh;
 }
 
 .container-view {
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  margin-top: 4vh;
   padding-left: 2vh;
 }
 
