ddb1558cec5cfe0d25d87e2dfefb11c845b4af7e
Emili Puh <epuh@unipu.hr>
2024-02-16 17:15:25 +0100

    Dodane metode za dodavanje prihoda i pregled prihoda(samo ispis u konzoli)

--- src/services/index.js
+++ src/services/index.js
@@ -1,26 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 // glavna instanca pomoću koje ćemo komunicirati sa backendom
 let Service = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 1000
-}) //metoda koja instancira novu varijablu za komunikaciju sa određenim backendom
+  baseURL: "http://localhost:3000",
+  timeout: 2000,
+}); //metoda koja instancira novu varijablu za komunikaciju sa određenim backendom
 
-// sve metode koje su vezane za dohvat stanja
+// sve metode koje su vezane za dohvat stanja sa backenda
 let Stanje = {
-    async dohvatiStanje() {
-        let response = await Service.get('/');
-        let data = response.data;
-        console.log("Podaci s backenda: ", data);
-        return data;
-    },
-    async noviPrihod(data) { // prima nove podatke koji se trebaju spremiti
-        console.log("Spremam prihod podatke na backend...", data)
-    },
-    async noviRashod(data) { // prima nove podatke koji se trebaju spremiti
-        console.log("Spremam rashod podatke na backend...", data)
-    },
+  // ovom metodom dohvaćam podatke iz monga i prikazujem ih u konzoli
+  async dohvatiStanje() {
+    let response = await Service.get("/");
+    let data = response.data;
+    console.log("Podaci s backenda: ", data);
+    return data;
+  },
+};
 
-}
+// metode koje se odnose na prihode
+let Prihod = {
+  async noviPrihod(podaci) {
+    let response = await Service.post("/noviPrihod", podaci);
+    let spremljeniPodaci = response.data;
+    return spremljeniPodaci;
+  },
+  async pregledPrihoda() {
+    console.log("Opet OK");
+    let response = await Service.get("/pregledPrihoda");
+    let data = response.data;
+    console.log("Prihodi sa backenda: ", data);
+    return data;
+  }
+};
 
-export { Service, Stanje }
+// metode koje se odnose na rashode
+let Rashod = {
+  async noviRashod(data) {
+    // prima nove podatke koji se trebaju spremiti
+    console.log("Spremam rashod podatke na backend...", data);
+  },
+};
+
+export { Service, Stanje, Prihod, Rashod };
