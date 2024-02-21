b87a12510226dbedc2fa891a66204eb951d2d508
Emili Puh <epuh@unipu.hr>
2024-02-21 16:57:53 +0100

    Dodane metode za novi rashod i pregled rashoda

--- src/services/index.js
+++ src/services/index.js
@@ -8,62 +8,93 @@ let Service = axios.create({
 
 let Stanje = {
   async dohvatiStanje() {
-    let response = await Service.get("/");
-    let data = response.data;
-    let stanje = {
-      iznos: null,
-      prihodi: null,
-      rashodi: null,
-    };
-    stanje.prihodi += data[0].prihodi;
-    console.log("Ukupni prihodi: ", stanje.prihodi);
-    stanje.rashodi += data[0].rashodi;
-    console.log("Ukupni rashodi: ", stanje.rashodi);
-    stanje.iznos = stanje.prihodi - stanje.rashodi;
-    console.log("Ukupno stanje računa: ", stanje.iznos);
-    console.log("Podaci sa backenda: ", stanje);
-
-    return stanje;
+    try {
+      let response1 = await Service.get("/");
+      let data = response1.data; // objekt
+      data = {
+        stanjeRacuna: null,
+        prihodi: data[0].prihodi,
+        rashodi: data[0].rashodi,
+      };
+      // tu dobijem stanje iz baze koje je inicijalizirano na 0
+      // moram dohvatiti podatke sa prihoda i svaki od iznosa pribrojiti stanju baze(koji je inicijaliziran na 0)
+      let response2 = await Service.get("/pregledPrihoda");
+      let prihodi = response2.data;
+      console.log("Dohvaćeni prihodi: ", prihodi);
+      // moram proći kroz sve zapise i pribrojiti njihove iznose stanju
+      for (let i = 0; i < prihodi.length; i++) {
+        // tu moram napraviti da mi se od pojedinog prihoda iznos izračuna u stanje
+        let prihod = prihodi[i];
+        let iznosPrihoda = prihod.iznos;
+        data.prihodi += iznosPrihoda;
+        data.stanjeRacuna = data.prihodi - data.rashodi;
+      }
+      let response3 = await Service.get("/pregledRashoda");
+      let rashodi = response3.data;
+      console.log("Dohvaćeni rashodi: ", rashodi);
+      for (let i = 0; i < rashodi.length; i++){
+        let rashod = rashodi[i];
+        let iznosRashoda = rashod.iznos;
+        data.rashodi += iznosRashoda;
+        data.stanjeRacuna = data.prihodi - data.rashodi;
+      } 
+      return data;
+    } catch (error) {
+      console.error("Greška prilikom dohvata stanja:", error);
+      throw error;
+    }
   },
 };
 
 // TU MORAMO OBLIKOVATI PODATKE ZA BACKEND!!!!!!
 let Prihod = {
-  async noviPrihod(data) {
+  async noviPrihod(podaciPrihod) {
     // prilagođavamo podatke backendu
     let serverData = {
-      // ovako mi se podaci spremaju na backend...
-      // za pojednostaviti, kopiram podatke koji mi se "vuču" sa frontenda
-      kategorija: data.kategorija,
-      iznos: data.iznos,
-      datum: data.datum,
-      biljeska: data.biljeska
+      kategorija: podaciPrihod.kategorija,
+      iznos: parseFloat(podaciPrihod.iznos),
+      datum: podaciPrihod.datum,
+      biljeska: podaciPrihod.biljeska,
     };
 
-    // napravimo logiku za ažuriranje prihoda i stanja
-    let prihod = 0;
-    prihod += parseInt(serverData.iznos);
-    console.log("Prihod: ", prihod)
-
     // čekamo odgovor sa backenda
-    await Service.post("/noviPrihod", serverData)
-
+    let response = await Service.post("/noviPrihod", serverData);
+    let prihod = response.data;
+    console.log("Data iz prihoda sa service.post: ", prihod.iznos); // vraća mi upisani iznos u obliku floata
     // vraćamo se natrag
-    return 
+    return;
   },
   async pregledPrihoda() {
-    console.log("Opet OK");
     let response = await Service.get("/pregledPrihoda");
     let data = response.data;
     console.log("Prihodi sa backenda: ", data);
     return data;
   },
+  async obrisiPrihod(prihod_id) {},
 };
 
 let Rashod = {
-  async noviRashod(data) {
-    // prima nove podatke koji se trebaju spremiti
-    console.log("Spremam rashod podatke na backend...", data);
+  async noviRashod(podaciRashod) {
+    // prilagođavamo podatke backendu
+    let serverData = {
+      kategorija: podaciRashod.kategorija,
+      iznos: parseFloat(podaciRashod.iznos),
+      datum: podaciRashod.datum,
+      biljeska: podaciRashod.biljeska,
+    };
+    console.log("Server data: ", serverData);
+    // čekamo odgovor sa backenda
+    let response = await Service.post("/noviRashod", serverData);
+    let rashod = response.data;
+    console.log("Data iz rashoda sa service.post: ", rashod.iznos); // vraća mi upisani iznos u obliku floata
+    // vraćamo se natrag
+    return;
+  },
+  async pregledRashoda() {
+    let response = await Service.get("/pregledRashoda");
+    let data = response.data;
+    console.log("Rashodi sa backenda: ", data);
+    return data;
   },
 };
 
