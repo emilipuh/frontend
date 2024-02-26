b93a0638a78802db40c4ba899aa7cb9bce28b582
Emili Puh <epuh@unipu.hr>
2024-02-26 12:30:42 +0100

    Dodana ruta za prikaz detalja pojedinog rashoda

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,67 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import DodavanjePrihoda from '@/views/DodavanjePrihoda';
-import DodavanjeRashoda from '@/views/DodavanjeRashoda';
-import PregledPrihoda from '@/views/PregledPrihoda';
-import DetaljiPrihoda from '@/views/DetaljiPrihoda'
-import PregledRashoda from '@/views/PregledRashoda';
-import Stednja from '@/views/Stednja';
-import DodajStednju from '@/views/DodajStednju';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import DodavanjePrihoda from "@/views/DodavanjePrihoda";
+import DodavanjeRashoda from "@/views/DodavanjeRashoda";
+import PregledPrihoda from "@/views/PregledPrihoda";
+import DetaljiPrihoda from "@/views/DetaljiPrihoda";
+import PregledRashoda from "@/views/PregledRashoda";
+import DetaljiRashoda from "@/views/DetaljiRashoda";
+import Stednja from "@/views/Stednja";
+import DodajStednju from "@/views/DodajStednju";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/dodajPrihod',
-    name: 'dodajPrihod',
-    component: DodavanjePrihoda
+    path: "/dodajPrihod",
+    name: "dodajPrihod",
+    component: DodavanjePrihoda,
   },
   {
-    path: '/dodajRashod',
-    name: 'dodajRashod',
-    component: DodavanjeRashoda
+    path: "/dodajRashod",
+    name: "dodajRashod",
+    component: DodavanjeRashoda,
   },
   {
-    path: '/pregledPrihoda',
-    name: 'pregledPrihoda',
-    component: PregledPrihoda
+    path: "/pregledPrihoda",
+    name: "pregledPrihoda",
+    component: PregledPrihoda,
   },
   {
-    path: '/detaljiPrihoda/:id',
-    name: 'detaljiPrihoda',
+    path: "/detaljiPrihoda/:id",
+    name: "detaljiPrihoda",
     component: DetaljiPrihoda,
-    props: true
-  },  
-  {
-    path: '/pregledRashoda',
-    name: 'pregledRashoda',
-    component: PregledRashoda
+    props: true,
   },
   {
-    path: '/stednja',
-    name: 'stednja',
-    component: Stednja
+    path: "/detaljiRashoda/:id",
+    name: "detaljiRashoda",
+    component: DetaljiRashoda,
+    props: true,
   },
   {
-    path: '/dodajStednju', 
-    name: 'dodajStednju',
-    component: DodajStednju
-  }
-]
+    path: "/pregledRashoda",
+    name: "pregledRashoda",
+    component: PregledRashoda,
+  },
+  {
+    path: "/stednja",
+    name: "stednja",
+    component: Stednja,
+  },
+  {
+    path: "/dodajStednju",
+    name: "dodajStednju",
+    component: DodajStednju,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
