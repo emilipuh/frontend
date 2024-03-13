973dae15c9789087bf4a9be99d54b49acc0554b4
Emili Puh <epuh@unipu.hr>
2024-03-13 21:46:56 +0100

    Dodan view za ulaz u app

--- src/views/Pocetna.vue
+++ src/views/Pocetna.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="container">
-    <div class="content">
-      <button @click="prijava()" class="btn">PRIJAVA</button>
-      <button @click="registracija()" class="btn">REGISTRACIJA</button>
-    </div>
+    <h1 class="title" :class="{ centered: !showButtons }">EXPENSE TRACK</h1>
+    <Transition name="fade-in">
+      <div class="buttons" v-show="showButtons">
+        <button @click="prijava()" class="btn-login">PRIJAVA</button>
+        <button @click="registracija()" class="btn-signup">REGISTRACIJA</button>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script>
 export default {
   name: "pocetna",
+  data() {
+    return {
+      showButtons: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showButtons = true;
+    }, 2000); 
+  },
   methods: {
     prijava() {
       this.$router.push({ name: "login" });
@@ -22,26 +35,50 @@ export default {
 </script>
 
 <style scoped lang="scss">
-// centriranje u sredinu prikaza
 .container {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
-  margin: auto;
-  min-height: 100vh;
+  min-height: 100dvh;
 }
 
-.content {
-  border-radius: 2vh;
-  background-color: #066995;
-  display: flex;
-  flex-direction: column;
-  padding: 5vh 5vh 3vh 5vh;
+.title {
+  margin-top: 10vh; 
+  transition: margin-top 1s;
+  font-size: 40px;
+  color: #ffbe53;
 }
-.btn {
-  background-color: yellow;
-  padding: 1vh 3vh;
+
+.centered {
+  margin-top: 40vh;
+}
+
+.buttons {
+  margin-top: 65vh;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn-login, .btn-signup {
+  border-radius: 1vh;
+  border: none;
+  background-color: rgb(223, 223, 19);
+  padding: 2vh 3vh;
   font-size: 18px;
-  margin-bottom: 2vh;
+}
+.btn-login {
+  padding: 0px 5vh;
+}
+
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: opacity 2s;
+}
+
+.fade-in-enter-from,
+.fade-in-leave-to {
+  opacity: 0;
 }
 </style>
