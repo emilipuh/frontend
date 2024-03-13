3e7c62e8254855c40f88edbc651201cf932cec8f
Emili Puh <epuh@unipu.hr>
2024-03-13 21:40:23 +0100

    Dodan Auth modul za autentifikaciju, dodani interceptori i izmjene u kodu radi filtriranja prikaza upisa

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import $router from "@/router";
 
 // glavna instanca pomoću koje ćemo komunicirati sa backendom
 let Service = axios.create({
@@ -6,42 +7,55 @@ let Service = axios.create({
   timeout: 2000,
 });
 
+Service.interceptors.request.use((request) => {
+  try {
+    request.headers["Authorization"] = "Bearer " + Auth.getToken();
+  } catch (e) {
+    console.error(e);
+  }
+  return request;
+});
+// može se dogoditi da token istekne i da se vrati 401, tako da je dobra praksa napraviti još jedan interceptor
+
+Service.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status == 401 || error.response.status == 403) {
+      Auth.logout();
+      $router.go();
+    }
+  }
+);
+
 let Stanje = {
-  async dohvatiStanje() {
+  async dohvatiStanje(userId) {
     try {
-      let response1 = await Service.get("/");
+      userId = Auth.getUserId();
+      console.log("userId: ", userId);
+      let response1 = await Service.get(`/${userId}`);
       let data = response1.data; // objekt
-      data = {
-        stanjeRacuna: null,
-        prihodi: data[0].prihodi,
-        rashodi: data[0].rashodi,
-        stednja: data[0].stednja,
-      };
       // tu dobijem stanje iz baze koje je inicijalizirano na 0
       // moram dohvatiti podatke sa prihoda i svaki od iznosa pribrojiti stanju baze(koji je inicijaliziran na 0)
-      let response2 = await Service.get("/pregledPrihoda");
+      let response2 = await Service.get(`/pregledPrihoda/${userId}`);
       let prihodi = response2.data;
-      console.log("Dohvaćeni prihodi: ", prihodi);
       // moram proći kroz sve zapise i pribrojiti njihove iznose stanju
       for (let i = 0; i < prihodi.length; i++) {
         // tu moram napraviti da mi se od pojedinog prihoda iznos izračuna u stanje
         let prihod = prihodi[i];
         let iznosPrihoda = prihod.iznos;
         data.prihodi += iznosPrihoda;
-        data.stanjeRacuna = data.prihodi - data.rashodi;
+        data.stanje = data.prihodi - data.rashodi;
       }
-      let response3 = await Service.get("/pregledRashoda");
+      let response3 = await Service.get(`/pregledRashoda/${userId}`);
       let rashodi = response3.data;
-      console.log("Dohvaćeni rashodi: ", rashodi);
       for (let i = 0; i < rashodi.length; i++) {
         let rashod = rashodi[i];
         let iznosRashoda = rashod.iznos;
         data.rashodi += iznosRashoda;
-        data.stanjeRacuna = data.prihodi - data.rashodi;
+        data.stanje = data.prihodi - data.rashodi;
       }
-      let response4 = await Service.get("/pregledStednji");
+      let response4 = await Service.get(`/pregledStednji/${userId}`);
       let stednje = response4.data;
-      console.log("Dohvaćene štednje: ", stednje);
       for (let i = 0; i < stednje.length; i++) {
         let stednja = stednje[i];
         let iznosStednje = parseFloat(stednja.iznos);
@@ -50,7 +64,7 @@ let Stanje = {
       return data;
     } catch (error) {
       console.error("Greška prilikom dohvata stanja:", error);
-      throw error;
+      return;
     }
   },
 };
@@ -59,31 +73,34 @@ let Stanje = {
 let Prihod = {
   async noviPrihod(podaciPrihod) {
     // prilagođavamo podatke backendu
+    let userId = Auth.getUserId();
     let serverData = {
+      userId: userId,
       kategorija: podaciPrihod.kategorija,
       iznos: parseFloat(podaciPrihod.iznos),
       datum: podaciPrihod.datum,
       biljeska: podaciPrihod.biljeska,
     };
-
+    console.log("server data: ", serverData);
     // čekamo odgovor sa backenda
-    let response = await Service.post("/noviPrihod", serverData);
-    let prihod = response.data;
-    console.log("Data iz prihoda sa service.post: ", prihod.iznos); // vraća mi upisani iznos u obliku floata
+    await Service.post("/noviPrihod", serverData);
     // vraćamo se natrag
     return;
   },
-  async pregledPrihoda() {
-    let response = await Service.get("/pregledPrihoda");
-    let data = response.data;
-    console.log("Prihodi sa backenda: ", data);
-    return data;
+  async pregledPrihoda(userId) {
+    try {
+      let response = await Service.get(`/pregledPrihoda/${userId}`); // Dodajte userId kao dio rute
+      let data = response.data;
+      return data;
+    } catch (error) {
+      console.error("Greška prilikom dohvata prihoda:", error);
+      return;
+    }
   },
   async dohvatiPrihod(id) {
     console.log("ID: ", id);
-    let response = await Service.get(`detaljiPrihoda/${id}`);
+    let response = await Service.get(`/detaljiPrihoda/${id}`);
     let data = response.data;
-    console.log("Detalji prihoda: ", data);
     return data;
   },
   async obrisiPrihod(id) {
@@ -98,32 +115,33 @@ let Prihod = {
 
 let Rashod = {
   async noviRashod(podaciRashod) {
+    let userId = Auth.getUserId();
     // prilagođavamo podatke backendu
     let serverData = {
+      userId: userId,
       kategorija: podaciRashod.kategorija,
       iznos: parseFloat(podaciRashod.iznos),
       datum: podaciRashod.datum,
       biljeska: podaciRashod.biljeska,
     };
-    console.log("Server data: ", serverData);
     // čekamo odgovor sa backenda
-    let response = await Service.post("/noviRashod", serverData);
-    let rashod = response.data;
-    console.log("Data iz rashoda sa service.post: ", rashod.iznos); // vraća mi upisani iznos u obliku floata
+    await Service.post("/noviRashod", serverData);
     // vraćamo se natrag
     return;
   },
-  async pregledRashoda() {
-    let response = await Service.get("/pregledRashoda");
-    let data = response.data;
-    console.log("Rashodi sa backenda: ", data);
-    return data;
+  async pregledRashoda(userId) {
+    try {
+      let response = await Service.get(`/pregledRashoda/${userId}`); // Dodajte userId kao dio rute
+      let data = response.data;
+      return data;
+    } catch (error) {
+      console.error("Greška prilikom dohvata rashoda:", error);
+      return;
+    }
   },
   async dohvatiRashod(id) {
-    console.log("ID: ", id);
     let response = await Service.get(`detaljiRashoda/${id}`);
     let data = response.data;
-    console.log("Detalji rashoda: ", data);
     return data;
   },
   async obrisiRashod(id) {
@@ -137,27 +155,31 @@ let Rashod = {
 };
 
 let Stednja = {
-  async dohvatiStednje() {
-    let response = await Service.get("/pregledStednji");
-    let data = response.data;
-    console.log("Štednje sa backenda: ", data);
-    return data;
+  async pregledStednji(userId) {
+    try {
+      let response = await Service.get(`/pregledStednji/${userId}`); // Dodajte userId kao dio rute
+      let data = response.data;
+      return data;
+    } catch (error) {
+      console.error("Greška prilikom dohvata štednji:", error);
+      return;
+    }
   },
   async novaStednja(podaciStednja) {
+    let userId = Auth.getUserId();
+
     let serverData = {
+      userId: userId,
       iznos: parseFloat(podaciStednja.iznos),
       datum: podaciStednja.datum,
       biljeska: podaciStednja.biljeska,
     };
-    console.log("Server data: ", serverData);
-    let podaci = await Service.post("/novaStednja", podaciStednja);
-    console.log(podaci);
+    await Service.post("/novaStednja", serverData);
+    return;
   },
   async dohvatiStednju(id) {
-    console.log("ID: ", id);
     let response = await Service.get(`detaljiStednje/${id}`);
     let data = response.data;
-    console.log("Detalji stednje: ", data);
     return data;
   },
   async obrisiStednju(id) {
@@ -170,4 +192,57 @@ let Stednja = {
   },
 };
 
-export { Service, Stanje, Prihod, Rashod, Stednja };
+let Auth = {
+  async registration(user_data) {
+    let response = await Service.post("/korisnici", user_data);
+    console.log("Rezultat registracije: ", response.data);
+    return true;
+  },
+  async login(username, password) {
+    let response = await Service.post("/auth", {
+      username: username,
+      password: password,
+    });
+    let user = response.data;
+    localStorage.setItem("user", JSON.stringify(user));
+    return true;
+  },
+  logout() {
+    localStorage.removeItem("user");
+  },
+  getUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  },
+  getUserId() {
+    let user = Auth.getUser();
+    if (user && user.id) {
+      return user.id;
+    } else {
+      return false;
+    }
+  },
+  getToken() {
+    let user = Auth.getUser();
+    if (user && user.token) {
+      return user.token;
+    } else {
+      return false;
+    }
+  },
+  state: {
+    get username() {
+      let user = Auth.getUser();
+      if (user) {
+        return user.username;
+      }
+    },
+    get email() {
+      let user = Auth.getUser();
+      if (user) {
+        return user.email;
+      }
+    },
+  },
+};
+
+export { Service, Stanje, Prihod, Rashod, Stednja, Auth };
