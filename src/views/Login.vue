2e2334c5073315f05480499ba05c957eb1c074a4
Emili Puh <epuh@unipu.hr>
2024-03-13 21:44:19 +0100

    Dodana metoda za login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,14 +3,15 @@
     <h1>LOGIN</h1>
     <div class="content">
       <form>
-        <div class="email">
-          <label for="email">Email:</label>
+        <div class="username">
+          <label for="username">Korisničko ime:</label>
           <div>
             <input
-              placeholder="Email"
-              type="email"
+              placeholder="Korisničko ime"
+              type="username"
               class="form-control"
-              id="email"
+              id="username"
+              v-model="username"
             />
           </div>
         </div>
@@ -22,39 +23,70 @@
               type="password"
               class="form-control"
               id="password"
+              v-model="password"
             />
           </div>
         </div>
       </form>
+      <p v-if="error" class="error-message">
+        <i class="fa-solid fa-triangle-exclamation"></i>
+        {{ error }}
+      </p>
     </div>
-    <button type="submit" class="btn">LOGIN</button>
+    <button type="button" @click="login()" class="btn">LOGIN</button>
   </div>
 </template>
 
 <script>
+import { Auth } from "@/services";
+
 export default {
   name: "login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      error: "",
+    };
+  },
+  methods: {
+    async login() {
+      if (!this.username || !this.password) {
+        this.error = "Korisničko ime i lozinka su obavezni";
+        return;
+      }
+      let success = await Auth.login(this.username, this.password);
+      console.log("Rezultat prijave: ", success);
+      if (success == true) {
+        this.$router.push({ name: "home" });
+      }
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
 .container {
-  min-height: 70dvh;
-  max-height: 100dvh;
-  display: flex;
-  justify-content: center;
+  margin: auto;
+  min-height: 100dvh;
   color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
 h1 {
   color: black;
-  text-align: start;
-  margin: 0vh 0vh 2vh 4vh;
+  margin-bottom: 3vh;
+  align-self: start;
+  margin-left: 10vh;
 }
 .content {
   background-color: #066995;
   border-radius: 2vh;
   margin: 0vh 4vh 3vh 4vh;
+  padding: 0vh 2vh;
   min-height: 30dvh;
   display: flex;
   flex-direction: column;
@@ -63,10 +95,10 @@ h1 {
 }
 
 label {
-  margin-bottom: 1vh;
+  margin-bottom: 0.5vh;
 }
 
-.email,
+.username,
 .password {
   text-align: start;
   font-size: 18px;
@@ -82,8 +114,7 @@ label {
   background-color: yellow;
   color: black;
   font-size: 18px;
-  padding: 2vh;
-  margin: 0vh 10vh;
+  padding: 2vh 8vh;
   border-radius: 1.5vh;
 }
 </style>
