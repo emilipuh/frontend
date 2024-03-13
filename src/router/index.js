342bae612d8be4b9f6cb4a27895f1dcf35453d2a
Emili Puh <epuh@unipu.hr>
2024-03-13 21:38:21 +0100

    Dodan router.beforeEach i ruta za početnu stranicu

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,27 @@ import DetaljiRashoda from "@/views/DetaljiRashoda";
 import PregledStednji from "@/views/PregledStednji";
 import DodavanjeStednje from "@/views/DodavanjeStednje";
 import DetaljiStednje from "@/views/DetaljiStednje";
+import Login from "@/views/Login";
+import SignUp from "@/views/SignUp";
+import { Auth } from "@/services/index.js";
+import Pocetna from "@/views/Pocetna.vue";
 
 const routes = [
+  {
+    path: "/pocetna",
+    name: "pocetna",
+    component: Pocetna,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
+  },
   {
     path: "/",
     name: "home",
@@ -69,6 +88,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const javneStranice = ["/pocetna", "/login", "/signup"]; //stranice na koje možemo doći bez logina
+  const potrebanLogin = !javneStranice.includes(to.path); //to => objekt, path => "url, putanja" stranice na koju želimo doći => url trenutne stranice
+  // login je potreban kada stranica na koju želimo doći(to.path) nije u javnim stranicama
+  const user = Auth.getUser();
+  if (potrebanLogin && !user) {
+    // ako je login potreban i ne postoji korisnik uvijek me vrati na login ili signup
+    next("/pocetna");    
+    return;
+  }
+  next();
 });
 
 export default router;
