ce2b45b5b9c3d7282f442f8bef6e90b1c6047252
Emili Puh <epuh@unipu.hr>
2024-03-20 12:21:39 +0100

    Dodan button za uređivanje korisničkih podataka

--- src/components/KorisnickiPodaci.vue
+++ src/components/KorisnickiPodaci.vue
@@ -21,21 +21,32 @@
           <p style="color: #0585bc; font-weight: bold">Email:</p>
           <p>{{ auth.email }}</p>
         </div>
+        <div class="modal-footer">
+          <div class="uredi-korisnika">
+            <i class="fa-solid fa-user-pen icon"></i>
+            <button class="button" @click="korisnik()">Uredi korisnika</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Auth } from '@/services';
+import { Auth } from "@/services";
 
 export default {
   name: "KorisnickiPodaci",
   data() {
     return {
-      auth: Auth.state
-    }
-  }
+      auth: Auth.state,
+    };
+  },
+  methods: {
+    korisnik() {
+      this.$router.push({ name: "urediKorisnika" });
+    },
+  },
 };
 </script>
 
@@ -44,11 +55,31 @@ export default {
   max-width: 300px;
   margin: auto;
 }
-
 .modal-body {
-  padding: 20px;
-  margin: 30px;
+  padding: 3vh;
+  margin: 4vh 4vh 1vh 4vh;
   border-radius: 8px;
   background-color: white;
+}
+.modal-footer {
+  border: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.uredi-korisnika {
+  background-color: whitesmoke;
+  margin: 1vh 0 2vh 0;
+  padding: 0.5vh;
+  border-radius: 1vh;
+  max-width: 22vh;
+}
+.button {
+  background-color: inherit;
+  border: none;
+}
+.icon {
+  padding-left: 0.5vh;
 }
 </style>
